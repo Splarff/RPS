7a4c33d2b252e4ef63bcfcc69f498f6bdc06e003
Splarff <smithjacob1012@gmail.com>
2023-01-18 11:23:37 -0600

    about to attempt radical changes, revert if needed

--- java.js
+++ java.js
@@ -32,9 +32,94 @@ function playRound(playerSelection, computerSelection) {
     }else if ((playerSelection.toUpperCase() == "SCISSORS") && (computerSelection == "Paper")){
         return "You Win!";
     }
+    else {
+        return "You goofed the code bot";
+    }
   }
    
   const playerSelection = "Rock";
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
   console.log(playerSelection, computerSelection)
+
+  // 5 rounds using for loops
+  // keep score via 2 variables with "++"
+  //end loop with else containing "You won/lost x-y" 
+  let playerScore = 0;
+  let ComputerScore = 0;
+
+  function game(){
+    for (let i = 0; i < 5; i++) {
+        if (i === 0){
+            playRound(playerSelection, computerSelection)
+                if (playRound(playerSelection, computerSelection) == "You Win!"){
+                    console.log(playerScore + ComputerScore);
+                    return playerScore++;
+                } else if (playRound(playerSelection, computerSelection) == "You Lose!"){
+                    console.log(playerScore + ComputerScore);
+                    return ComputerScore++;
+                } else {
+                    console.log(playerScore + ComputerScore);
+                    return "Draw";
+                }
+        }   else if (i === 1){
+            playRound(playerSelection, computerSelection)
+                if (playRound(playerSelection, computerSelection) == "You Win!"){
+                    console.log(playerScore + ComputerScore);
+                    return playerScore++;
+                } else if (playRound(playerSelection, computerSelection) == "You Lose!"){
+                    console.log(playerScore + ComputerScore);
+                    return ComputerScore++;
+                } else {
+                    console.log(playerScore + ComputerScore);
+                   return "Draw";
+                }
+
+        }   else if (i === 2){
+            playRound(playerSelection, computerSelection)
+                if (playRound(playerSelection, computerSelection) == "You Win!"){
+                    console.log(playerScore + ComputerScore);
+                    return playerScore++;
+                    
+                } else if (playRound(playerSelection, computerSelection) == "You Lose!"){
+                    console.log(playerScore + ComputerScore);
+                    return ComputerScore++;
+                    
+                } else {
+                    console.log(playerScore + ComputerScore);
+                   return "Draw";
+                }
+
+        }   else if (i === 3){
+            playRound(playerSelection, computerSelection)
+                if (playRound(playerSelection, computerSelection) == "You Win!"){
+                    console.log(playerScore + ComputerScore);
+                    return playerScore++;
+                } else if (playRound(playerSelection, computerSelection) == "You Lose!"){
+                    console.log(playerScore + ComputerScore);
+                    return ComputerScore++;
+                } else {
+                    console.log(playerScore + ComputerScore);
+                    return "Draw";
+                }
+
+        }    else if (i === 4){
+            playRound(playerSelection, computerSelection)
+                if (playRound(playerSelection, computerSelection) == "You Win!"){
+                    console.log(playerScore + ComputerScore);
+                    return playerScore++;
+                } else if (playRound(playerSelection, computerSelection) == "You Lose!"){
+                    console.log(playerScore + ComputerScore);
+                    return ComputerScore++;
+                } else {
+                    console.log(playerScore + ComputerScore);
+                   return "Draw";
+                }
+
+        }   else {
+            console.log(i)
+            return playerScore.toString + ComputerScore.toString;
+        }
+     }
+}
+game()
