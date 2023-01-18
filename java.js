4f980c1e8c13c78a9d840b31bdd6722affaea571
Splarff <smithjacob1012@gmail.com>
2023-01-18 08:28:58 -0600

    Added "win/lose/draw" conditions

--- java.js
+++ java.js
@@ -1,16 +1,40 @@
-function GetComputerChoice(choice) { 
-    let i = Math.random();
+function getComputerChoice(choice) { 
+    const i = Math.random();
     if (i <= .33){
-        return "rock";
+        return "Rock";
     } else if (i >.33 && i <= .66) {
-        return "paper";
+        return "Paper";
     } else if (i >.66){
-        return "scissors";
+        return "Scissors";
     } else {
         return "error"
     }
-} console.log(GetComputerChoice())
+}
 // option = rock/paper/scissors
 //rock <= .33 ; .33 < paper <= .66 ; scissors >.66
 // use math.random to determine option value
 // use if statements to determine rock/paper/scissors
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toUpperCase() == computerSelection.toUpperCase()) {
+        return "Draw";
+    }
+    else if ((playerSelection.toUpperCase() == "ROCK") && (computerSelection == "Paper")) {
+        return "You Lose!";
+    }else if ((playerSelection.toUpperCase() == "ROCK") && (computerSelection == "Scissors")){
+        return "You Win!";
+    }else if ((playerSelection.toUpperCase() == "PAPER") && (computerSelection == "Rock")){
+        return "You Win!";
+    }else if ((playerSelection.toUpperCase() == "PAPER") && (computerSelection == "Scissors")){
+        return "You Lose!";
+    }else if ((playerSelection.toUpperCase() == "SCISSORS") && (computerSelection == "Rock")){
+        return "You Lose!";
+    }else if ((playerSelection.toUpperCase() == "SCISSORS") && (computerSelection == "Paper")){
+        return "You Win!";
+    }
+  }
+   
+  const playerSelection = "Rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  console.log(playerSelection, computerSelection)
